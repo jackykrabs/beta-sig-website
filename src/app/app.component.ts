87372ad43bba5d84ab101e52947e4ec5bb7a3a3d
Jack Allen <jackykrabs@gmail.com>
2021-02-04 23:59:00 -0600

    complete most of home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'beta-sig-site';
+  title = 'Beta Sigma Psi - Iota Chapter';
 }
